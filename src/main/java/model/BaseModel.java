92ae661eede9889a75cbcecc4c6781b8a816c5be
sebastiankurant <sebastian.kt7@gmail.com>
2017-05-14 17:53:25 +0200

    Add model class
    Create and add base model class

---
+++ src/main/java/model/BaseModel.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.lang.reflect.Field;
+
+public class BaseModel {
+
+	protected Integer id;
+	protected String name;
+
+	public BaseModel(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Gets name.
+	 *
+	 * @return Value of name.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets new name.
+	 *
+	 * @param name New value of name.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Gets id.
+	 *
+	 * @return Value of id.
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * Sets new id.
+	 *
+	 * @param id New value of id.
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		for (Field field : this.getClass().getDeclaredFields()) {
+			field.setAccessible(true);
+			Object value = null;
+			try {
+				value = field.get(this);
+				if (value != null) {
+					sb.append(field.getName() + ":" + value + ",");
+				}
+			} catch (IllegalAccessException e) {
+				System.out.println("Can't access object.");
+			}
+		}
+		return sb.toString();
+	}
+}
