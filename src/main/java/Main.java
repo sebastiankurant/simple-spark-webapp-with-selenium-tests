af5f71c79bd5caf19760247a894b99355bc9369f
sebastiankurant <sebastian.kt7@gmail.com>
2017-05-14 18:32:49 +0200

    Add routes
    Add routes and paths

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,81 @@
 import controller.EventController;
+import dao.SqliteJDBCConnector;
 import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.sql.SQLException;
 
-import  static spark.Spark.*;
+import static spark.Spark.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        EventController eventController = new EventController();
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
         port(8888);
-
-        // Always start with more specific routes
-        get("/hello", (req, res) -> "Hello World");
-
-        // Always add generic routes to the end
-        get("/", EventController::renderProducts, new ThymeleafTemplateEngine());
+        try {
+            SqliteJDBCConnector.createTables();
+        } catch (SQLException e) {
+            System.out.println("Cant load database");
+            e.printStackTrace();
+            System.exit(1);
+        }
         // Equivalent with above
-        get("/index", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render( EventController.renderProducts(req, res) );
+        path("/index", () -> {
+            get("", (Request request, Response response) ->
+                    new ThymeleafTemplateEngine().render(eventController.renderEvents()));
+
+            get("/:id", (Request request, Response response) ->
+                    new ThymeleafTemplateEngine().render(eventController.renderEvent(request)));
+
+            get("/category/", (Request request, Response response) ->
+                    new ThymeleafTemplateEngine().render(eventController.renderEventsBy(request)));
+
+            path("/manager/", () -> {
+                get("", (Request request, Response response) ->
+                        new ThymeleafTemplateEngine().render(eventController.displayManager()));
+
+                get("new/", (Request request, Response response) ->
+                        new ThymeleafTemplateEngine().render(eventController.displayEventCreator()));
+
+                get("edit/:id", (Request request, Response response) ->
+                        new ThymeleafTemplateEngine().render(eventController.displayEventEditor(request)));
+
+                get("remove/:id", (Request request, Response response) -> {
+                    eventController.removeEvent(request);
+                    response.redirect("/index/manager/");
+                    return null;
+                });
+
+                get("newCategory/", (Request request, Response response) ->
+                        new ThymeleafTemplateEngine().render(eventController.displayCategoryCreator()));
+
+                post("new/", (Request request, Response response) -> {
+                    eventController.createEvent(request);
+                    response.redirect("/index/manager/");
+                    return null;
+                });
+
+                post("newCategory/", (Request request, Response response) -> {
+                    eventController.createCategory(request);
+                    response.redirect("/index/manager/");
+                    return null;
+                });
+
+                post("edit/:id", (Request request, Response response) -> {
+                    eventController.editProduct(request);
+                    response.redirect("/index/manager/");
+                    return null;
+                });
+            });
+        });
+
+        get("*", (request, response) -> {
+            System.out.println("404 not found!!");
+            return new ThymeleafTemplateEngine().render(eventController.render404());
         });
     }
-
-
 }
